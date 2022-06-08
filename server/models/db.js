dee7e1d771c94644fd3520970f44087e3eceb211
smile <h3298543073@outlook.com>
2022-06-08 19:59:18 +0800

    modified db.js

--- server/models/db.js
+++ server/models/db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose')
-mongoose.connect('mongodb://127.0.0.1:27017/meituan')
+mongoose.connect('mongodb://localhost:27017/meituan')
     .then(() => console.log('连接数据库成功...'))
     .catch(err => console.error('连接数据库失败' + err))
 
